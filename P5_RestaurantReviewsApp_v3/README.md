450b67d912b745d1d166446c5d2be5d3a7a69877
Kimberly Hung <36671443+kymmbear@users.noreply.github.com>
2019-02-16 00:40:31 -0500

    Add files via upload

---
+++ P5_RestaurantReviewsApp_v3/README.md
@@ -0,0 +1,14 @@
+# Restaurant Reviews App Project
+
+The purpose of this project was to add functionality and responsiveness to a page that contains restaurant reviews. To view this project, download or clone the project on to your computer, set up the HTTP server as indicated in the original project documentation, and visit the [site](http://localhost:8000)
+
+###Instructions from original project file:
+Check the version of Python you have: python -V. If you have Python 2.x, spin up the server with python -m SimpleHTTPServer 8000 (or some other port, if port 8000 is already in use.) For Python 3.x, you can use python3 -m http.server 8000. If you don't have Python installed, navigate to Python's [website](https://www.python.org/) to download and install the software.
+
+## Leaflet.js and Mapbox:
+
+This repository uses [leafletjs](https://leafletjs.com/) with [Mapbox](https://www.mapbox.com/).
+
+### Contributions
+
+Original project file found [here](https://github.com/udacity/mws-restaurant-stage-1)
---
+++ P5_RestaurantReviewsApp_v3/sw.js
@@ -0,0 +1,71 @@
+
+
+self.addEventListener('install', function(event) {
+  event.waitUnitl(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(cacheFiles);
+    })
+  );
+});
+
+const cacheFiles = [
+    './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './js/sw_registration.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+];
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+   event.respondWith(
+     caches.match(event.request).then(function(response){
+       if(response) {
+         console.log('Found ', event.request, ' in cache');
+         return response;
+       }
+       else {
+         console.log('Could not find ', event.request, ' in cache, fetching...');
+         return fetch(event.request)
+         .then(function(response) {
+           const clonedResponse = response.clone();
+            caches.open('v1').then(function(cache) {
+              cache.put(event.request, response);
+            })
+            return response;
+         })
+         .catch(function(err){
+           console.error(err);
+         });
+       }
+     })
+   );
+ });
